ddba93b6bd95d3dd4a948ef295909d6b363381d8
vanoop218184 <ankit5737v@gmail.com>
2024-06-17 19:03:16 +0530

    vercel.json file added

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const path =require('path')
+const path = require('path');
 
 require("./db/config");
 const User = require("./db/User.js");
-const Product=require("./db/Product.js")
+const Product = require("./db/Product.js");
+
 app.use(express.json());
 app.use(cors());
+
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
@@ -16,67 +18,66 @@ app.post("/register", async (req, res) => {
   res.send(result);
 });
 
-app.get("/",(req,res)=>{
-  app.use(express.static(path.resolve(__dirname,"frontend","build")));
-  res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
+
 app.post("/login", async (req, res) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-        res.send(user);
-      } 
-    }  
+      res.send(user);
+    } 
+  }  
 });
-app.post("/addproduct",async(req,res)=>{
-  let product=new Product(req.body);
-  let result=await product.save();
 
+app.post("/addproduct", async (req, res) => {
+  let product = new Product(req.body);
+  let result = await product.save();
   res.send(result);
+});
 
-})
-app.get("/products", async(req,res)=>{
-  let result= await Product.find();
-  if(result.length>0)
-  {
+app.get("/products", async (req, res) => {
+  let result = await Product.find();
+  if (result.length > 0) {
     res.send(result);
+  } else {
+    res.send({ result: "No product found" });
   }
-  else{
-    res.send({result:"No product found"});
-  }
-})
-app.delete("/product/:id",async(req,res)=>{
-   
-   const result= await Product.deleteOne({_id:req.params.id});
-   res.send(result);
-})
-app.get("/product/:id",async(req,res)=>{
-  const result= await Product.findOne({_id:req.params.id});
-  if(result)
-  {res.send(result);}
-  else{
-    res.send({result:"No record Found"});
-  }
-})
-app.put("/product/:id",async(req,res)=>{
-  let result=await Product.updateOne(
-    {_id:req.params.id},
-    {
-      $set:req.body
-    }
-    )
+});
+
+app.delete("/product/:id", async (req, res) => {
+  const result = await Product.deleteOne({ _id: req.params.id });
+  res.send(result);
+});
+
+app.get("/product/:id", async (req, res) => {
+  const result = await Product.findOne({ _id: req.params.id });
+  if (result) {
     res.send(result);
-})
-app.get("/search/:key",async(req,res)=>{
-  const result=await Product.find({
-    "$or":[
-      {name:{$regex:req.params.key}},
-      {company:{$regex:req.params.key}},
-      {category:{$regex:req.params.key}}
+  } else {
+    res.send({ result: "No record found" });
+  }
+});
+
+app.put("/product/:id", async (req, res) => {
+  let result = await Product.updateOne(
+    { _id: req.params.id },
+    { $set: req.body }
+  );
+  res.send(result);
+});
+
+app.get("/search/:key", async (req, res) => {
+  const result = await Product.find({
+    "$or": [
+      { name: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } }
     ]
   });
   res.send(result);
-})
-app.listen("8000", () => {
-  console.log("app is running at port 8000");
 });
+
+module.exports = app; // Export the app
